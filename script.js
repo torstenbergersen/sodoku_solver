7a849e89e98527b63897967a5fb529360665d505
Torsten Bergersen <52758041+torstenbergersen@users.noreply.github.com>
2024-11-18 11:31:04 -0500

    Clean up

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const sudokuGrid = document.body.querySelector(".sudoku-grid");
 
+// populate grid with empty cells
 for (let i = 0; i < 81; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.textContent = "#";
+    cell.textContent = "";
     sudokuGrid.appendChild(cell);
 }
+
+// checks for solvability and uniqueness
+function solver() {}
+
+const createGrid = () => {};
+
+const newPuzzle = document.body.querySelector("#new-puzzle");
+newPuzzle.addEventListener("click", () => {
+    createGrid();
+});
+
+// visualizes solving algorithm within grid
+function solverVisualizer() {}
+
+createGrid();
