54d297a11ee4f40d6a9e7226464d0fbcce3f954d
Torsten Bergersen <52758041+torstenbergersen@users.noreply.github.com>
2024-11-19 10:29:28 -0500

    Get things looking like a sudoku board

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const sudokuGrid = document.body.querySelector(".sudoku-grid");
-
-// populate grid with empty cells
 for (let i = 0; i < 81; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     cell.textContent = "";
     sudokuGrid.appendChild(cell);
 }
-
 // checks for solvability and uniqueness
 function solver() {}
 
-const createGrid = () => {};
+const createGrid = () => {
+    // three sets for row column and square units, remove used number from each set at each step in creating grid (constraint propogation)
+    // would need 9 sets for each row, column, and square
+    // feasible to make 27 sets?
+
+    // how to iterate throught he cells?
+
+    let cells = sudokuGrid.children;
+    for (let i = 0; i < cells.length; i++) {
+        let cellInstance = cells[i];
+        cellInstance.textContent = Math.floor(Math.random() * (9 - 1) + 1);
+    }
+};
 
 const newPuzzle = document.body.querySelector("#new-puzzle");
 newPuzzle.addEventListener("click", () => {
